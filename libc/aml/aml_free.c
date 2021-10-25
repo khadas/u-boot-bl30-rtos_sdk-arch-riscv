043d4f4c900ca3fa9e90a4e95c3a81555504a271
Xiaohu.Huang <xiaohu.huang@amlogic.com>
2021-10-25 15:42:53 +0800

    Arch/riscv: Move libc dir to libs dir for riscv to RTOS SDK [1/1]
    PD#SWPL-61628
    
    Problem:
    Move libc dir to libs dir for riscv to RTOS SDK.
    
    Solution:
    Move libc dir to libs dir for riscv to RTOS SDK.
    
    Verify:
    T5D_AM301
    
    Change-Id: Iafc2c66485770753d8b0a721b4e64fd518124319
    Signed-off-by: Xiaohu.Huang <xiaohu.huang@amlogic.com>

--- libc/aml/aml_free.c
+++
@@ -1,7 +0,0 @@
-#include "aml_free.h"
-#include <FreeRTOS.h>
-
-void free(void *ptr)
-{
-	vPortFree(ptr);
-}
--- libc/aml/aml_malloc.c
+++
@@ -1,8 +0,0 @@
-#include "aml_malloc.h"
-#include <FreeRTOS.h>
-
-void *malloc(size_t size)
-{
-	return pvPortMalloc(size);
-}
-
