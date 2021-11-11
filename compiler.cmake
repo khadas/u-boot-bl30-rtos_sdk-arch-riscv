5780a2c3ee81159b1d009237c219b730037aa204
Xiaohu.Huang <xiaohu.huang@amlogic.com>
2021-11-11 14:36:33 +0800

    arch/riscv: Use the PRODUCT_VERSION_MSG as VERSION for product. [1/1]
    PD#SWPL-61470
    
    Problem:
    Use the PRODUCT_VERSION_MSG as VERSION for product.
    
    Solution:
    Pass the PRODUCT_VERSION_MSG as VERSION to aocpu product.
    
    Verify:
    T5W_AT301
    
    Change-Id: I5148daa536c6db60b86877a84f9c7b2c17893e96
    Signed-off-by: Xiaohu.Huang <xiaohu.huang@amlogic.com>

--- compiler.cmake
+++ compiler.cmake
@@ -8,10 +8,12 @@ if(CONFIG_LIBC_STD)
 set(linker_flags "${linker_flags},--wrap=_malloc_r,--wrap=_free_r,--wrap=_realloc_r")
 endif()
 
+add_definitions(-DVERSION=\"${PRODUCT_VERSION}\")
+
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=rv32imc -mabi=ilp32 -DN200_REVA=1 -D__ASM -DSOC_t5d -Dgcc -imacros${AUTOCONF_H}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O2 -g -ffunction-sections -fdata-sections -fno-common -fgnu89-inline")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --specs=nano.specs --specs=nosys.specs")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=rv32imc -mabi=ilp32 -nostdlib -DN200_REVA=1 -DSOC_t5d -Dgcc -DVERSION=\"2021\" -imacros${AUTOCONF_H}")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=rv32imc -mabi=ilp32 -nostdlib -DN200_REVA=1 -DSOC_t5d -Dgcc -imacros${AUTOCONF_H}")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O2 -g -ffunction-sections -fdata-sections -fno-common -fno-builtin -fgnu89-inline")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --specs=nano.specs --specs=nosys.specs")
 set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -march=rv32imc -mabi=ilp32 -DN200_REVA=1 -DSOC_t5d -Dgcc -imacros${AUTOCONF_H}")
