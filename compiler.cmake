99f3fa3f9a7309d8f98b73e33c634872a7414bf0
Xiaohu.Huang <xiaohu.huang@amlogic.com>
2021-11-12 14:42:27 +0800

    arch/riscv: Generate PRODUCT_VERSION var and pass to product. [1/1]
    PD#SWPL-61470
    
    Problem:
    Generate Generate PRODUCT_VERSION var and pass to product.
    
    Solution:
    Generate Generate PRODUCT_VERSION var and pass to product.
    
    Verify:
    T5W_AT301
    
    Change-Id: I6c8fb7a99f2cb33712c201881d5121d5e72553ef
    Signed-off-by: Xiaohu.Huang <xiaohu.huang@amlogic.com>

--- compiler.cmake
+++ compiler.cmake
@@ -8,7 +8,9 @@ if(CONFIG_LIBC_STD)
 set(linker_flags "${linker_flags},--wrap=_malloc_r,--wrap=_free_r,--wrap=_realloc_r")
 endif()
 
-add_definitions(-DVERSION=\"${PRODUCT_VERSION}\")
+execute_process(COMMAND ${SDK_BASE}/scripts/version_generator.sh ${SDK_BASE}/products/${PRODUCT} OUTPUT_VARIABLE PRODUCT_VERSION_MSG OUTPUT_STRIP_TRAILING_WHITESPACE)                                                                                                                
+add_definitions(-DVERSION=\"${PRODUCT_VERSION_MSG}\")
+message(STATUS "product version:${PRODUCT_VERSION_MSG}")
 
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=rv32imc -mabi=ilp32 -DN200_REVA=1 -D__ASM -DSOC_t5d -Dgcc -imacros${AUTOCONF_H}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O2 -g -ffunction-sections -fdata-sections -fno-common -fgnu89-inline")
