85662ad1dcd0d32f2c1421d3c7649628153b814a
Xiaohu.Huang <xiaohu.huang@amlogic.com>
2021-11-18 14:28:59 +0800

    arch/riscv: Integrated the difference of T5W codes. [1/1]
    PD#SWPL-61630
    
    Problem:
    Integrated the difference of T5W codes.
    
    Solution:
    Integrated the difference of T5W codes.
    Base src_ao commit：79a4856 i2c: add i2c support
    
    Verify:
    T5W_AT301/T5W_AT309
    
    Change-Id: Ie9f324cb3b1275bfa085728c20076d6a500a82ac
    Signed-off-by: Xiaohu.Huang <xiaohu.huang@amlogic.com>

--- compiler.cmake
+++ compiler.cmake
@@ -12,13 +12,13 @@ execute_process(COMMAND ${SDK_BASE}/scripts/version_generator.sh ${SDK_BASE}/pro
 add_definitions(-DVERSION=\"${PRODUCT_VERSION_MSG}\")
 message(STATUS "product version:${PRODUCT_VERSION_MSG}")
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=rv32imc -mabi=ilp32 -DN200_REVA=1 -D__ASM -DSOC_t5d -Dgcc -imacros${AUTOCONF_H}")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=rv32imc -mabi=ilp32 -DN200_REVA=1 -D__ASM -DSOC_${CONFIG_SOC_NAME} -D${COMPILER} -imacros${AUTOCONF_H}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O2 -g -ffunction-sections -fdata-sections -fno-common -fgnu89-inline")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --specs=nano.specs --specs=nosys.specs")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=rv32imc -mabi=ilp32 -nostdlib -DN200_REVA=1 -DSOC_t5d -Dgcc -imacros${AUTOCONF_H}")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=rv32imc -mabi=ilp32 -nostdlib -DN200_REVA=1 -DSOC_${CONFIG_SOC_NAME} -D${COMPILER} -imacros${AUTOCONF_H}")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O2 -g -ffunction-sections -fdata-sections -fno-common -fno-builtin -fgnu89-inline")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --specs=nano.specs --specs=nosys.specs")
-set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -march=rv32imc -mabi=ilp32 -DN200_REVA=1 -DSOC_t5d -Dgcc -imacros${AUTOCONF_H}")
+set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -march=rv32imc -mabi=ilp32 -DN200_REVA=1 -DSOC_${CONFIG_SOC_NAME} -D${COMPILER} -imacros${AUTOCONF_H}")
 set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -O2 -g -ffunction-sections -fdata-sections -fno-common -fgnu89-inline")
 set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} --specs=nano.specs --specs=nosys.specs")
 
